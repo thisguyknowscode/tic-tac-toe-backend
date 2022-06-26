8ddb0ad8b61fbf7264d2caa82c0ddb3d58d855c0
Michael Snyder <thisguyknowscode@gmail.com>
2022-06-26 12:13:06 -0400

    DB via migrations

--- tictactoe/models.py
+++ tictactoe/models.py
@@ -1,3 +1,42 @@
+from datetime import datetime
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class ttt_users(models.Model):
+    class Meta:
+        indexes = [
+            models.Index(fields=['username', ],)
+        ]
+    id = models.IntegerField(primary_key='id', default=0, unique=True)
+    username = models.CharField(unique=True, max_length=255)
+    registeredOn = models.DateTimeField(default=datetime.now)
+
+
+class ttt_game(models.Model):
+    class Meta:
+        indexes = [
+            models.Index(fields=['winnerUserId', ]),
+        ]
+    id = models.IntegerField(primary_key=True, unique=True)
+    winnerUserId = models.ForeignKey(
+        to=ttt_users, to_field='id', on_delete=models.CASCADE)
+    startedOn = models.DateTimeField(default=datetime.now)
+    endedOn = models.DateTimeField(default=datetime.now)
+
+
+class ttt_move(models.Model):
+    class Meta:
+        indexes = [
+            models.Index(fields=['gameId', ]),
+            models.Index(fields=['userId', ]),
+        ]
+    id = models.IntegerField(primary_key=True, unique=True)
+    gameId = models.ForeignKey(
+        ttt_game, on_delete=models.CASCADE, default=-1)
+    userId = models.ForeignKey(
+        ttt_users, on_delete=models.CASCADE, default=-1)
+    moveX = models.SmallIntegerField(default=-1)
+    moveY = models.SmallIntegerField(
+        default=-1, validators=[MinValueValidator(-1), MaxValueValidator(2)])
+    movedOn = models.DateTimeField(default=datetime.now)
